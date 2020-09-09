7a1d941ecded85feddc54bc36d018ca0d67c4cc3
Brent Gorwin <brent.gorwin@gmail.com>
2020-09-08 21:58:52 -0500

    updated the about page with an image and sizing.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,119 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<v-container>
+		<v-parallax
+			dark
+			height="820"
+			src="../img/about-parallax-background.png"
+			:aspect-ratio="16 / 9"
+		>
+			<v-row align="center" justify="center">
+				<v-col class="text-center" cols="12">
+					<h1 class="font-weight-thin parallax-title">About Coffee Passport</h1>
+					<h4 class="subheading about-sub-heading">
+						Make supporting local coffee shops in your city easy!
+					</h4>
+				</v-col>
+			</v-row>
+		</v-parallax>
+		<v-row id="second-section">
+			<v-col xs="12" sm="12" md="6" lg="6">
+				<v-img id="about-image" src="../img/default-img.jpg"></v-img>
+			</v-col>
+			<v-col id="about-writeup" xs="12" sm="12" md="6" lg="6">
+				<blockquote class="blockquote">
+					Coffee Passpord is an application that helps coffee lovers find the
+					perfect coffee ship while in the city they're living in or traveling
+					through. Each coffee shop has a rating, address and image to help with
+					finding which one suits your coffee needs.
+				</blockquote>
+			</v-col>
+		</v-row>
+
+		<v-row id="third-row">
+			<v-col
+				v-for="about in abouts"
+				:key="`${about.title}`"
+				xs="12"
+				sm="12"
+				md="6"
+				lg="4"
+				class="d-flex flex-wrap"
+			>
+				<v-card max-width="344" class="mx-auto">
+					<v-list-item>
+						<v-list-item-content>
+							<v-list-item-title class="headline text-center">{{
+								about.title
+							}}</v-list-item-title>
+						</v-list-item-content>
+					</v-list-item>
+					<v-img
+						src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+						height="194"
+					></v-img>
+					<v-card-text>{{ about.description }}</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			abouts: [
+				{
+					title: 'Discover New Shops',
+					description:
+						'When in a new city, discover high quality coffee shops that you may not find on other apps'
+				},
+				{
+					title: 'Rated Coffee Shop',
+					description:
+						'Find shops that are only highly rated by users who have been there'
+				},
+				{
+					title: 'Why Use Coffee-Passport',
+					description:
+						'Coffee-Passpord is a list of highly rated coffee shops where coffee shop owners have a passion for bringing you the finest coffee roasting for the perfect cup'
+				}
+			]
+		};
+	}
+};
+</script>
+
+<style scoped>
+.parallax-title {
+	font-size: 60px;
+}
+
+.about-sub-heading {
+	font-size: 30px;
+}
+
+#second-section {
+	margin: 15px 10% 15px 10%;
+}
+#about-image {
+	max-height: 380px;
+	max-width: 380px;
+	border-radius: 50%;
+	position: relative;
+	display: block;
+	margin: 4% auto 4% auto;
+}
+
+#about-writeup {
+	padding: 12% 5% 10% 5%;
+	text-align: center;
+}
+
+#third-row {
+	background-color: #f1f3f8;
+	height: 0 auto;
+	padding-top: 10%;
+	padding-bottom: 10%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,75 @@
 <template>
-  <v-container>
-    <v-row id="home-heading">
-      <v-col cols="12" justify="center">
-        <div>
-          <v-img class="overlay" src="../img/home-page-background.jpg" max-height="270">
-            <h1 class="austin-heading text-center">Coffee Passport</h1>
-          </v-img>
-        </div>
-      </v-col>
-    </v-row>
+	<v-container>
+		<v-row id="home-heading">
+			<v-col cols="12" justify="center">
+				<div>
+					<v-img
+						class="overlay"
+						src="../img/home-page-background.jpg"
+						max-height="270"
+					>
+						<h1 class="austin-heading font-weight-thin text-center">
+							Coffee Passport
+						</h1>
+					</v-img>
+				</div>
+			</v-col>
+		</v-row>
 
-    <v-row>
-      <v-col cols="12" max-height="200px">
-        <div>
-          <h1 class="text-center">Choose A City</h1>
-          <div class="d-flex flex-wrap justify-center">
-            <v-btn
-              class="mb-4 ma-4"
-              v-for="city in cities"
-              :key="city"
-              outlined
-              color="teal"
-              dark
-            >{{ city }}</v-btn>
-          </div>
-        </div>
-      </v-col>
-    </v-row>
+		<v-row>
+			<v-col cols="12" max-height="200px">
+				<div>
+					<h1 class="text-center">Choose A City</h1>
+					<div class="d-flex flex-wrap justify-center">
+						<v-btn
+							class="mb-4 ma-4"
+							v-for="city in cities"
+							:key="city"
+							outlined
+							color="teal"
+							dark
+							>{{ city }}</v-btn
+						>
+					</div>
+				</div>
+			</v-col>
+		</v-row>
 
-    <v-row class="d-flex flex-direction-row">
-      <v-col class="d-flex flex-row flex-wrap justify-center order-2" cols="12">
-        <div class="mr-4 mb-6" v-for="shop in shops" :key="`${shop.company}`">
-          <CoffeeCards :shop="shop" />
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+		<v-row class="d-flex flex-direction-row">
+			<v-col class="d-flex flex-row flex-wrap justify-center order-2" cols="12">
+				<div class="mr-4 mb-6" v-for="shop in shops" :key="`${shop.company}`">
+					<CoffeeCards :shop="shop" />
+				</div>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
-import CoffeeCards from "../components/CoffeeCards";
-import shopData from "../data/shops.json";
+import CoffeeCards from '../components/CoffeeCards';
+import shopData from '../data/shops.json';
 
 export default {
-  components: {
-    CoffeeCards
-  },
-  data() {
-    return {
-      shops: shopData,
-      cities: ["Austin", "Dallas", "San Francisco", "Los Angesles"]
-    };
-  }
+	components: {
+		CoffeeCards
+	},
+	data() {
+		return {
+			shops: shopData,
+			cities: ['Austin', 'Dallas', 'San Francisco', 'Los Angesles']
+		};
+	}
 };
 </script>
 
 <style scoped>
 .border {
-  border: 1px solid red;
+	border: 1px solid red;
 }
 
 .austin-heading {
-  margin-top: 100px;
-  color: white;
-  font-size: 60px;
+	margin-top: 100px;
+	color: white;
+	font-size: 60px;
 }
 </style>
